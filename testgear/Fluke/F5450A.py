e66bf183598e1681e0ffe87d00a22d0f13b8a12e
Philipp Cochems <philipp.cochems@gmail.com>
2021-03-29 13:14:55 +0200

    Fluke 5450A tested

--- testgear/Fluke/F5450A.py
+++ testgear/Fluke/F5450A.py
@@ -3,6 +3,14 @@
 import testgear.base_classes as base
 
 class F5450A(base.calibrator):
+    def reset(self):
+        self.write("CLEAR;")
+
+
+    def get_status(self):
+        return self.query("STATUS;")
+
+
     def guard(self, state):
         """Guard configuration:
             True -> Guard connected to LO
@@ -14,9 +22,13 @@ class F5450A(base.calibrator):
             self.write("EXT GUARD ON;")
 
 
-    def set_value(self, value):
+    def set_value(self, value=None, open=False):
         """set output voltage"""
-        self.write("OUTPUT {0:0.8g};".format(value))
+        if open:
+            self.write("OPEN;")
+        else:    
+            self.write("OUTPUT {0:d};".format(value))
+        return self.get_value()
 
 
     def get_value(self):
