a10cb136ad0b214218e7b6c0e3cb014a132fa63e
Philipp Cochems <philipp.cochems@gmail.com>
2021-03-29 12:34:47 +0200

    Fluke 5450A base functions

--- testgear/Fluke/F5450A.py
+++ testgear/Fluke/F5450A.py
@@ -3,60 +3,31 @@
 import testgear.base_classes as base
 
 class F5450A(base.calibrator):
-
-    def output(self, state):
-        if state:
-            self.write("OPER")
-        else:
-            self.write("STBY")
-
     def guard(self, state):
         """Guard configuration:
             True -> Guard connected to LO
             False -> Guard disconnected from LO
         """
         if state:
-            self.write("IGRD")
+            self.write("EXT GUARD OFF;")
         else:
-            self.write("EGRD")
-
-
-    def div(self, state):
-        """Divider configuration:
-            True -> Output divided
-            False -> Output not divided
-        """
-        if state:
-            self.write("DIVY")
-        else:
-            self.write("DIVN")
-
-
-    def external_sense(self, state):
-        """4W configuration:
-            True -> external sense (4W)
-            False -> internal sense (2W)
-        """
-        if state:
-            self.write("ESNS")
-        else:
-            self.write("ISNS")
+            self.write("EXT GUARD ON;")
 
 
     def set_value(self, value):
         """set output voltage"""
-        self.write("SOUT {0:0.8g}; OPER".format(value))
+        self.write("OUTPUT {0:0.8g};".format(value))
 
 
     def get_value(self):
         """get actual output value"""
-        return float(self.query("GOUT"))
+        return float(self.query("VALUE;"))
 
 
     def get_function(self):
-        return 'VDC'
+        return 'OHMS'
 
 
     def get_functions(self):
-        return ['VDC']
+        return ['OHMS']
     
