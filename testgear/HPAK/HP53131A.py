f62d6a0559b9b40aa2d27d3b18e898e571290b44
Philipp Cochems <philipp.cochems@gmail.com>
2021-04-02 18:51:12 +0200

    cast frequency reading to float

--- testgear/HPAK/HP53131A.py
+++ testgear/HPAK/HP53131A.py
@@ -3,6 +3,6 @@ import testgear.base_classes as base
 
 class HP53131A(base.dmm):
     def get_reading(self, channel=None):
-        return self.query("READ?")
+        return float(self.query("READ?").strip())
 
 
