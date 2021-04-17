780897f3f821ecea01ffc5dfd84949bdc1e2d98a
Philipp Cochems <philipp.cochems@gmail.com>
2021-04-17 11:45:54 +0200

    ID check for HP3458A

--- testgear/HPAK/HP3458A.py
+++ testgear/HPAK/HP3458A.py
@@ -4,7 +4,13 @@ import testgear.base_classes as base
 class HP3458A(base.meter):
     def init(self):
         self.write("END ALWAYS")
+
+        if "HP3458A" not in self.query("ID?"):
+            print("no HP3458A detected!!")
+            return 1
+
         self.set_timeout(30)
+
     
 
     def get_reading(self):
