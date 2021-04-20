e07363904f982acc7e8a234c5e2e5645500ae6b7
Philipp Cochems <philipp.cochems@gmail.com>
2021-04-20 23:49:51 +0200

    added get_monitor_state()

--- testgear/HPAK/A34970A.py
+++ testgear/HPAK/A34970A.py
@@ -35,7 +35,9 @@ class A34970A(base.meter):
 
     def get_monitor_state(self):
         """returns state of the monitor"""
-        return bool(int(self.query("ROUT:MON:STAT?")))
+        state   = bool(int(self.query("ROUT:MON:STAT?")))
+        channel = int(self.query("ROUTe:MONitor?").split("@")[1].split(")")[0])
+        return channel, state
 
 
     def set_relay(self, channel, state):
@@ -52,6 +54,19 @@ class A34970A(base.meter):
         return set(map(int, slist))
 
 
+    def trigger_scan(self):
+        self.write("INIT")
+
+    def get_triggered_scan(self):
+        #gleiche Aufbereitung der Antwort wie in get_scan verwenden
+        return self.query("FETCH?")
+
+
+    def get_scan(self):
+        #dictionary einbauen
+        return self.query("READ?")
+
+
     def set_scanlist(self, slist:set):
         strlist = ""
         for chan in slist:
@@ -63,18 +78,28 @@ class A34970A(base.meter):
         self.write("ROUT:SCAN (@)")
         
 
-    def conf_function_DCV(self, range=None, nplc=20, AutoZero=True, HiZ=True, channel=101):
+    def conf_function_DCV(self, mrange=None, nplc=20, AutoZero=True, HiZ=True, channel=101):
         """configures the meter to measure DCV. if range=None the meter is set to Autorange"""
         slist = self.get_scanlist()
         slist.add(channel)
 
         self.write("CONF:VOLT:DC (@{0:d})".format(channel))
         
-        if range is None:
+        if mrange is None:
             self.write("VOLT:DC:RANGE:AUTO 1,(@{0:d})".format(channel))
         else:
-            self.write("VOLT:DC:RANGE {0:0.6f},(@{1:d})".format(range, channel))
+            self.write("VOLT:DC:RANGE {0:0.6f},(@{1:d})".format(mrange, channel))
 
-        self.write("VOLT:DC:NPLC (@{0:0.3f})".format(nplc, channel))
+        self.write("VOLT:DC:NPLC {0:0.3f},(@{1:d})".format(nplc, channel))
         
+        #AutoZero
+        #HiZ
+
         self.set_scanlist(slist)
+
+
+    def conf_function_Pt100(self, mrange=None, nplc=20, fourWire=True, R0=100, channel=101):
+        pass
+
+
+#hp34970a.query("CONFigure?") zeigt Konfigurationen der Scanliste
