d030c31ea08a1154032dc5758435c2b6d7331754
Philipp Cochems <philipp.cochems@gmail.com>
2021-07-22 12:52:10 +0200

    added default VISA string and baud rate

--- testgear/CoTech/LowThermalSwitch.py
+++ testgear/CoTech/LowThermalSwitch.py
@@ -5,7 +5,10 @@ import testgear.base_classes as base
 class LowThermalSwitch(base.instrument):
     def init(self):
         #hier die Serielle Schnittstelle konfigurieren. Baudrate etc.
-        pass
+        self.resource.baud_rate = 9600
     
+    def default_VISA(self):
+        return 'ASRL/dev/ttyUSB0::INSTR'
+
     def channel(self, channel=0):
         pass
