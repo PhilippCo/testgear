2f2fafa22f07bc2e8953128aa580bcb02558b880
Philipp Cochems <philipp.cochems@gmail.com>
2021-07-24 00:21:00 +0200

    bugfix in Almemo1030 object

--- testgear/Ahlborn/Almemo1030.py
+++ testgear/Ahlborn/Almemo1030.py
@@ -9,10 +9,10 @@ class Almemo1030(base.source):
         return 'ASRL/dev/ttyUSB0::INSTR'
 
     def get_temp(self):
-        self.ser.write("p")
-        self.ser.resource.read_raw()
-        data = self.ser.resource.read_raw()
-        self.ser.resource.read_raw()
+        self.write("p")
+        self.resource.read_raw()
+        data = self.resource.read_raw()
+        self.resource.read_raw()
 
         data = self.query("p")
         return float(str(data)[6:-10])
