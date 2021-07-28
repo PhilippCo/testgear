cb816e85ae380448f7d0ff01a25b1687b4c1112e
Philipp Cochems <philipp.cochems@gmail.com>
2021-07-28 13:58:15 +0200

    added channels for frequency counting

--- testgear/HPAK/HP53131A.py
+++ testgear/HPAK/HP53131A.py
@@ -3,6 +3,12 @@ import testgear.base_classes as base
 
 class HP53131A(base.meter):
     def get_reading(self, channel=None):
+
+        if channel == 1:
+            self.write(':SENSe:FUNCtion "FREQUENCY 1"')
+        elif channel == 2:
+            self.write(':SENSe:FUNCtion "FREQUENCY 2"')
+
         return float(self.query("READ?").strip())
 
     def set_gatetime(self, gatetime):
