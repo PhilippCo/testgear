740e50c55b78771aae3eacc3d6a639b06efa8f31
Philipp Cochems <philipp.cochems@gmail.com>
2021-07-28 23:25:29 +0200

    added idstr

--- testgear/HPAK/HP53131A.py
+++ testgear/HPAK/HP53131A.py
@@ -2,8 +2,11 @@
 import testgear.base_classes as base
 
 class HP53131A(base.meter):
-    def get_reading(self, channel=None):
+    def init(self):
+        self.idstr = self.query("*IDN?").strip()
 
+
+    def get_reading(self, channel=None):
         if channel == 1:
             self.write(':SENSe:FUNCtion "FREQUENCY 1"')
         elif channel == 2:
@@ -11,14 +14,18 @@ class HP53131A(base.meter):
 
         return float(self.query("READ?").strip())
 
+
     def set_gatetime(self, gatetime):
         pass
+
 
     def set_trigger_level(self, level):
         pass
 
+
     def set_100kHz_filter(self, state, channel=1):
         pass
+
 
     def set_DC_coupling(self, state, channel=1):
         pass
