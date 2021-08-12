869a24f98137657722bdd8e9578a897d0f7b2d94
Philipp Cochems <philipp.cochems@gmail.com>
2021-08-12 10:52:22 +0200

    added method placeholer

--- testgear/Fluke/F8508A.py
+++ testgear/Fluke/F8508A.py
@@ -6,5 +6,41 @@ class F8508A(base.meter):
     def init(self):
         pass
 
-    def get_reading(self):
+    def get_reading(self, channel=None):
+        self.__select_channel(channel)
         return float(self.query("RDG?"))
+
+
+    def __select_channel(self, channel):
+        #to be done
+        pass
+
+
+    def conf_function_DCV(self, mrange=1000, nplc=200, AutoZero=True, HiZ=True, channel=None):
+        """configures the meter to measure DCV. if range=None the meter is set to Autorange"""
+        pass
+
+
+    def conf_function_DCI(self, mrange=None, nplc=100, AutoZero=True, HiZ=True, channel=None):
+        """configures the meter to measure DCI. if range=None the meter is set to Autorange"""
+        pass
+
+
+    def conf_function_ACV(self, mrange=None, nplc=100, AutoZero=True, HiZ=True, channel=None):
+        """configures the meter to measure DCV. if range=None the meter is set to Autorange"""
+        pass
+
+
+    def conf_function_ACI(self, mrange=None, nplc=100, AutoZero=True, HiZ=True, channel=None):
+        """configures the meter to measure DCV. if range=None the meter is set to Autorange"""
+        pass
+
+
+    def conf_function_OHM2W(self, mrange=None, nplc=100, AutoZero=True, OffsetCompensation=True, channel=None):
+        """configures the meter to measure DCV. if range=None the meter is set to Autorange"""
+        pass
+
+
+    def conf_function_OHM4W(self, mrange=None, nplc=200, AutoZero=True, OffsetCompensation=True, channel=1):
+        """configures the meter to measure 4w resistance. if range=None the meter is set to Autorange"""
+        pass
