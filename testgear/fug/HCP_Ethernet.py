995d8c2ce2694a635747e89b4b79b771c44e2f6e
Philipp Cochems <philipp.cochems@gmail.com>
2021-11-01 15:33:58 +0100

    basic functionality implemented

--- testgear/fug/HCP_Ethernet.py
+++ testgear/fug/HCP_Ethernet.py
@@ -2,35 +2,45 @@
 
 import testgear.base_classes as base
 
+# HV Supply always answers with an error code. 
+# Therefore, all communication is via query not write
+
 class HCP_Ethernet(base.source):
-    pass
+    def init(self):
+        self.resource.read_termination  = "\r\n"
+        self.resource.write_termination = "\r\n"
+    
+
+    def ip2visa(self, ip):
+        """overload ip2visa for SOCKET communication"""
+        return "TCPIP0::{0:s}::2101::SOCKET".format(ip)
 
 
+    def set_output(self, voltage=None, current=None, enabled=True, frequency=None, resistance=None, channel=1):
+        if enabled:
+            self.query("F1")
+        else:
+            self.query("F0")
+
+        if voltage is not None:
+            self.query("U{0:0.3f}".format(voltage))
+        
+        if current is not None:
+            self.query("I{0:0.7f}".format(current))
 
 
+    def get_output(self, channel=1):
+        """return an object which reflects the output conditions"""
+        obj = base.output_status()
 
- 
-# # Codeschnipsel von altem Script
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect(("192.168.137.84", 2101))
+        if self.query(">DON?").split(":")[1] == '1':
+            obj.enabled = True
+        else:
+            obj.enabled = False
 
-# volt = 0
-# print(volt)
-# amp  = 1e-3
-# out  = 1
-
-# s.send(("I"+str(amp)+"\n").encode('utf-8'))
-# print(s.recv(4096))
-
-# s.send(bytes("U"+str(volt)+"\n", 'utf-8'))
-# print(s.recv(4096))
-
-# s.send(bytes("F"+str(out)+"\n", 'utf-8'))
-# print(s.recv(4096))
-
-# s.send(bytes(">m0?\n", 'utf-8'))
-# print(s.recv(4096))
-
-# s.send(bytes(">m1?\n", 'utf-8'))
-# print(s.recv(4096))
-# s.close
+        obj.set_voltage = float(self.query(">s0?").split(":")[1])
+        obj.set_current = float(self.query(">s1?").split(":")[1])
+        
+        obj.voltage = float(self.query(">m0?").split(":")[1])
+        obj.current = float(self.query(">m1?").split(":")[1])
+        return obj
