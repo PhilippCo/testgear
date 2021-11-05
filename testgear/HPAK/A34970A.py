ee5e685c2e505f0a76d25c68ad955b59313bedc8
Philipp Cochems <philipp.cochems@gmail.com>
2021-11-05 08:15:21 +0100

    added missing functions, but not ready to go

--- testgear/HPAK/A34970A.py
+++ testgear/HPAK/A34970A.py
@@ -5,10 +5,12 @@ import numpy as np
 
 class A34970A(base.meter):
     def init(self):
-        pass
+        self.idstr = self.query("*IDN?").strip()
+
 
     def default_VISA(self):
         return 'TCPIP::192.168.2.88::gpib0,9::INSTR'
+
 
     def reset(self):
         self.write("*RST")
@@ -64,16 +66,20 @@ class A34970A(base.meter):
     def trigger_scan(self):
         self.write("INIT")
 
+
     def __scan2dict(self, data):
         meas  = list(map(float, data.split(",")))
         slist = self.get_scanlist()
         return dict(zip(slist, meas))
 
+
     def get_triggered_scan(self):
         return self.__scan2dict(self.query("FETCH?"))
 
+
     def get_scan(self):
         return self.__scan2dict(self.query("READ?"))
+
 
     def set_scanlist(self, slist:set):
         strlist = ""
@@ -112,9 +118,100 @@ class A34970A(base.meter):
         
         self.set_scanlist(slist)
 
+    
+    def conf_function_DCI(self, channel=101, mrange=None, nplc=20, AutoZero=True):
+        """configures the meter to measure DCI. if range=None the meter is set to Autorange"""
+        slist = self.get_scanlist()
+        slist.add(channel)
 
-    def conf_function_Pt100(self, nplc=20, fourWire=True, R0=100, channel=101):
-        pass
+        self.write("CONF:CURRENT:DC (@{0:d})".format(channel))
+
+        self.set_scanlist(slist)
+        
+
+
+    def conf_function_ACV(self, channel=101, mrange=None, nplc=20, AutoZero=True):
+        """configures the meter to measure DCI. if range=None the meter is set to Autorange"""
+        slist = self.get_scanlist()
+        slist.add(channel)
+
+        self.write("CONF:VOLT:AC (@{0:d})".format(channel))
+
+        self.set_scanlist(slist)
+
+
+    def conf_function_ACI(self, channel=101, mrange=None, nplc=20, AutoZero=True):
+        """configures the meter to measure DCI. if range=None the meter is set to Autorange"""
+        slist = self.get_scanlist()
+        slist.add(channel)
+
+        self.write("CONF:CURRENT:AC (@{0:d})".format(channel))
+
+        self.set_scanlist(slist)
+
+
+    def conf_function_frequency(self, gatetime=1, channel=1):
+        """configures the meter to measure DCI. if range=None the meter is set to Autorange"""
+        slist = self.get_scanlist()
+        slist.add(channel)
+
+        self.write("CONF:FREQ (@{0:d})".format(channel))
+
+        self.set_scanlist(slist)
+
+
+    def conf_function_period(self, gatetime=1, channel=1):
+        """configures the meter to measure DCI. if range=None the meter is set to Autorange"""
+        slist = self.get_scanlist()
+        slist.add(channel)
+
+        self.write("CONF:period (@{0:d})".format(channel))
+
+        self.set_scanlist(slist)
+
+    
+    def conf_function_OHM2W(self, mrange=None, nplc=100, AutoZero=True, OffsetCompensation=True, channel=1):
+        """configures the meter to measure DCI. if range=None the meter is set to Autorange"""
+        slist = self.get_scanlist()
+        slist.add(channel)
+
+        self.write("CONF:RES (@{0:d})".format(channel))
+
+        self.set_scanlist(slist)
+
+
+    def conf_function_OHM4W(self, mrange=None, nplc=100, AutoZero=True, OffsetCompensation=True, channel=1):
+        """configures the meter to measure DCI. if range=None the meter is set to Autorange"""
+        slist = self.get_scanlist()
+        slist.add(channel)
+
+        self.write("CONF:FRES (@{0:d})".format(channel))
+
+        self.set_scanlist(slist)
+
+
+    def conf_function_temperature(self, mrange=None, nplc=100, AutoZero=True, OffsetCompensation=True, channel=1):
+        """configures the meter to measure DCI. if range=None the meter is set to Autorange"""
+        slist = self.get_scanlist()
+        slist.add(channel)
+
+        self.write("CONF:TEMP (@{0:d})".format(channel))
+
+        self.set_scanlist(slist)
+
+
+    def conf_function_totalize(self, mrange=None, nplc=100, AutoZero=True, OffsetCompensation=True, channel=1):
+        """configures the meter to measure DCI. if range=None the meter is set to Autorange"""
+        slist = self.get_scanlist()
+        slist.add(channel)
+
+        self.write("CONF:totalize (@{0:d})".format(channel))
+
+        self.set_scanlist(slist)
+
+    
+    def conf_pt100(self, channel=101):
+        self.conf_function_temperature(a)
 
 
 #hp34970a.query("CONFigure?") zeigt Konfigurationen der Scanliste
