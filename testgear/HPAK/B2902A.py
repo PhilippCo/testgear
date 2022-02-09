58efa9a27382f3f82787cb9d56a2f2b8e93f1ca0
Philipp Cochems <philipp.cochems@gmail.com>
2022-02-09 21:38:18 +0100

    added basix B2902A functions

--- testgear/HPAK/B2902A.py
+++ testgear/HPAK/B2902A.py
@@ -1,18 +1,5 @@
 """Agilent B2902A Precision Source/Measure Unit"""
 
-#######################################################################################################
-# To achieve non-root access to the USB TMC device (Ubuntu 20.04 LTS)
-#
-# sudo nano /etc/udev/rules.d/50-hpak.rules
-#
-# copy the following into the file:
-# SUBSYSTEMS=="usb", ATTRS{idVendor}=="0957", ATTRS{idProduct}=="8c18", GROUP="users", MODE="0666"
-#
-# reload the rules:
-# sudo udevadm control --reload
-#######################################################################################################
-
-
 import testgear.base_classes as base
 import numpy as np
 
@@ -22,11 +9,40 @@ class B2902A(base.source):
         self.idstr = self.query("*IDN?").strip()
 
     
-    def set_output(self, voltage=None, current=None, enabled=True, frequency=0, resistance=None, fourWire=False, channel=1):
-        pass
+    def set_output(self, voltage=10, current=10e-3, enabled=True, frequency=None, resistance=None, mode="voltage", channel=1):
+        if enabled:
+            self.write(":OUTP{0:d} ON".format(channel))
+        else:
+            self.write(":OUTP{0:d} OFF".format(channel))
+
+            
+        if mode == "current":
+            self.write(":SOUR{0:d}:FUNC:MODE CURR".format(channel))
+            self.write(":SENS{0:d}:VOLT:PROT {1:0.7f}".format(channel, voltage))
+            self.write(":SOUR{0:d}:CURR {1:0.7f}".format(channel, current))
+            
+        else: #voltage mode
+            self.write(":SOUR{0:d}:FUNC:MODE CURR".format(channel))
+            self.write(":SENS{0:d}:CURR:PROT {1:0.7f}".format(channel, current))
+            self.write(":SOUR{0:d}:VOLT {1:0.7f}".format(channel, voltage))
 
 
-    def get_output(self):
+    def get_output(self, channel=1):
         """return an object which reflects the output conditions"""
         obj = base.output_status()
+        obj.channel = channel
+
+        if self.query(":OUTP{0:d}?".format(channel)).strip() == "1":
+            obj.enabled = True
+
+            obj.set_voltage = float(self.query(":SOUR{0:d}:VOLT?".format(channel)).strip())
+            obj.set_current = float(self.query(":SOUR{0:d}:CURR?".format(channel)).strip())
+
+            obj.voltage = float(self.query(":MEAS:VOLT? (@{0:d})".format(channel)).strip())
+            obj.current = float(self.query(":MEAS:CURR? (@{0:d})".format(channel)).strip())
+            
+        else:
+            obj.enabled = False
+        
         return obj
+
