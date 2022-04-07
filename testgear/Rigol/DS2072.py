ba4b5e6b5ccb403c542efa29537641305e72d2e3
Philipp Cochems <philipp.cochems@gmail.com>
2022-04-07 23:34:17 +0200

    add autoscale

--- testgear/Rigol/DS2072.py
+++ testgear/Rigol/DS2072.py
@@ -1,5 +1,6 @@
 import testgear.base_classes as base
 import numpy as np
+import time
 
 class DS2072(base.scope):
     def init(self):
@@ -8,6 +9,12 @@ class DS2072(base.scope):
 
     def set_time_date(self):
         pass
+
+
+    def opc(self):
+        while True:
+            if self.query("*OPC?").strip() == "1":
+                break
 
 
     def screenshot(self, filename="screen.bmp"):
@@ -25,9 +32,18 @@ class DS2072(base.scope):
         self.write(":STOP")
 
     
-    def single(self):
+    def single(self, wait=True, forced=False):
         self.write(":SINGle")
+        self.opc()
+        if forced:
+            self.force_trigger()
 
+        if wait:
+            while True:
+                if self.query(":TRIGger:STATus?").strip() == "STOP":
+                    break
+
+        self.opc()
     
     def force_trigger(self):
         self.write(":TFORce")
@@ -55,3 +71,58 @@ class DS2072(base.scope):
 
         return t, s
 
+
+    def null_offset(self, channel=1):
+            vmax = float(self.query(":MEASure:VMAX? CHANnel{0:d}".format(channel)))
+            vmin = float(self.query(":MEASure:VMIN? CHANnel{0:d}".format(channel)))
+
+            if vmin+vmax > 1e9:
+                return False
+
+            self.write(":CHANnel{0:d}:OFFSet {1:0.6f}".format(channel, -1*np.mean([vmax, vmin]) ) )
+            
+            return True
+
+
+    def autoscale(self, channel=1):
+        #ToDo: check probe factor
+        self.write(":CHANnel{0:d}:DISPlay 1".format(channel))
+        self.opc()
+        probe = float(self.query(":CHANnel{0:d}:PROBe?".format(channel)))
+        self.single(forced=True)
+
+        if float(self.query(":MEASure:VMAX?")) > 1e30:
+            #print("out of range")
+            self.write(":CHANnel{0:d}:SCALe {1:0.6f}".format(channel, 10 ))
+            self.write(":CHANnel{0:d}:OFFSet {1:0.6f}".format(channel, 0 ))
+            self.opc()
+            self.single(forced=True)
+            time.sleep(1)
+            
+        while True:
+            scale = float(self.query(":CHANnel{0:d}:SCALe?".format(channel)))
+            vmax = float(self.query(":MEASure:VMAX? CHANnel{0:d}".format(channel)))
+            vmin = float(self.query(":MEASure:VMIN? CHANnel{0:d}".format(channel)))
+            span = vmax - vmin
+            
+            if (span * 2 / scale / 8) > 0.2:
+                #print("passe Offset an")
+                self.write(":CHANnel{0:d}:OFFSet {1:0.6f}".format(channel, -1*np.mean([vmax, vmin]) ) )
+            
+            #print(vmin + span/2)
+
+            if (span * 2 / scale / 8) > 0.6:
+                #print("set fine")
+                self.write(":CHANnel{0:d}:SCALe {1:0.6f}".format(channel, span / (8 * 0.8)  ))
+                break
+
+            if scale <= (500e-6 * probe):
+                #print("limit reached")
+                break
+                
+            self.write(":CHANnel{0:d}:SCALe {1:0.6f}".format(channel, scale/2 ))
+            self.single(forced=True)
+            
+            time.sleep(1)
+        
+        self.null_offset()
